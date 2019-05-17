efc3ca5f045ceb73eb696d75b1e5b5bc94339dc7
Anna-Monica <34393930+anna-monica@users.noreply.github.com>
2019-05-17 11:34:31 +0200

    removed Hello message

--- src/main/java/Process.java
+++ src/main/java/Process.java
@@ -5,18 +5,18 @@ import akka.actor.UntypedAbstractActor;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
 
-public class MyActor extends UntypedAbstractActor {
+public class Process extends UntypedAbstractActor {
 
     // Logger attached to actor
     private final LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);
 
-    public MyActor() {
+    public Process() {
     }
 
     // Static function creating actor
     public static Props createActor() {
-        return Props.create(MyActor.class, () -> {
-            return new MyActor();
+        return Props.create(Process.class, () -> {
+            return new Process();
         });
     }
 
@@ -28,7 +28,6 @@ public class MyActor extends UntypedAbstractActor {
             Members m = (Members) message;
             log.info("Received message with data: " + m.data);
             for (ActorRef actor : m.references) {
-//                actor.tell(new MyHelloMessage("Hello "+actor.path().name()+" my name is "+this.self().path().name()), this.getSelf());
                 actor.tell(new QuorumRequest(), this.getSelf());
                 log.info("Request: "+self().path().name()+" -> "+actor.path().name());
                 
@@ -38,19 +37,6 @@ public class MyActor extends UntypedAbstractActor {
         }
         else if (message instanceof QuorumResponse) {
             log.info("Response: "+self().path().name()+" <- "+getSender().path().name());
-        }else if (message instanceof MyHelloMessage) {
-            MyHelloMessage m = (MyHelloMessage) message;
-            log.info("Received: " + m.data);
-
-        }
-    }
-
-    static public class MyHelloMessage {
-
-        public final String data;
-
-        public MyHelloMessage(String data) {
-            this.data = data;
         }
     }
 
